1a19b3e393195b75468e299c159fdb0b4a7f896d
mandrasch <777278+mandrasch@users.noreply.github.com>
2021-07-23 10:15:02 +0000

    Deploying to gh-pages from  @ 938416c38dd77ab0a363d2b9e597ce6a56e25c92 🚀

---
+++ assets/scripts/main.js
@@ -0,0 +1,49 @@
+"use strict";
+// TODO: Use IIFE
+window.addEventListener('load', function (event) {
+
+    // TODO: remove duplicates
+
+    let mouseEnterListener = function(event){
+        console.log('Event: mouseenter', event.target);
+
+        // only apply it if we're in un-collapsed/desktop mode
+        let navBarTogglerEl = event.target.closest('.navbar').querySelector('.navbar-toggler');
+        // https://stackoverflow.com/a/4866269 - use getComputedStyle
+        if(window.getComputedStyle(navBarTogglerEl, null).display == 'block'){
+            return; // bail
+        }
+
+        event.target.setAttribute("aria-expanded", true);
+        event.target.classList.add('show');
+        event.target.querySelector('.dropdown-menu').classList.add('show');
+    }
+
+    let mouseLeaveListener = function(event){
+
+       // only apply it if we're in un-collapsed/desktop mode
+       let navBarTogglerEl = event.target.closest('.navbar').querySelector('.navbar-toggler');
+       // https://stackoverflow.com/a/4866269
+       console.log(navBarTogglerEl, window.getComputedStyle(navBarTogglerEl, null).display);
+       if(window.getComputedStyle(navBarTogglerEl, null).display == 'block'){
+           return; // bail
+       }
+
+        console.log('Event: mouseleave', event.target);
+        event.target.setAttribute("aria-expanded", false);
+        event.target.classList.remove('show');
+        event.target.querySelector('.dropdown-menu').classList.remove('show');
+    }
+    
+    let navItemsWithDropdowns = document.querySelectorAll('.navbar .nav-item.dropdown');
+    console.log('nav items (li) with dropdown found:',navItemsWithDropdowns);
+    navItemsWithDropdowns.forEach(function (el, index) {
+        el.addEventListener('mouseenter', function (event) {
+            mouseEnterListener(event);
+        }, false);
+        el.addEventListener('mouseleave', function (event) {
+            mouseLeaveListener(event);
+        }, false);
+        
+    });
+});
