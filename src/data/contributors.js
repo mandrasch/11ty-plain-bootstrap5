ff38c1f281bdd4a1e239ee23963fbe7d3a070379
456477617264 <ec557782@gmail.com>
2022-10-04 22:56:25 +0100

    Add some brief documentation

--- src/data/contributors.js
+++ src/data/contributors.js
@@ -1,5 +1,5 @@
-const Repo = "mandrasch/11ty-plain-bootstrap5"; // Define the repo to get contributors from
-const Cache = require("@11ty/eleventy-fetch");
+const Repo = "mandrasch/11ty-plain-bootstrap5"; // Change repo to change the contributors. NOTE: REPO MUST BE PUBLIC
+const Cache = require("@11ty/eleventy-fetch"); // Use fetch to get the contributors
 
 module.exports = async function () {
   return await Cache(`https://api.github.com/repos/${Repo}/contributors`, {
